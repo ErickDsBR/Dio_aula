5842bce124e87b3fbb631026bb8ece53e3f0c77b
ErickDsBR <erick.brasil2102@gmail.com>
2026-03-12 11:26:45 -0300

    rodadas

--- src/index.js
+++ src/index.js
@@ -53,6 +53,7 @@ const players = [
     },
 ]
 
+
 async function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -62,33 +63,65 @@ async function randomplayer() {
     const randomIndex = [...players].sort(() => Math.random() - 0.5)
     return {
         player1: randomIndex[0],
-        player2: randomIndex[1]
-    } 
+        player2: randomIndex[1],
+        
+        
+    };
+    
+}
 
-};
 async function rounds() {
+    const {player1, player2} = await randomplayer();
+    
     for(let round = 1; round <= 5; round++) {
-        await wait(3000);
+        let block = await getRandomBlock();
+        await randomplayer();
+        await wait(5000);
+        console.log(`🏁 Corrida de Kart entre ${player1.name} e ${player2.name} Esta começando!... \n`)
         console.log(`\n 🏁 Rodada ${round} 🏁 \n`);
-        await rollDice();
+        await wait(5000);
+        console.log(`\n Bloco: ${block}  \n`)
+        await wait(4000);
+        console.log(`\n Jogador ${player1.name} lançando o dado... \n`);
+        let diceResult1 = await rollDice();
+
+        await wait(5000);
+        console.log(`\n Jogador ${player2.name} lançando o dado... \n`);
+        let diceResult2 = await rollDice();
         
     }
 };
 
-
 async function rollDice() {
-    return { 
-        value: Math.floor(Math.random() * 6) + 1,
-        console: console.log(`🎲 O dado foi lançado e o valor é: ${Math.floor(Math.random() * 6) + 1} 🎲`)
+    const value = Math.floor(Math.random() * 6) + 1;
+    console.log(`🎲 O dado foi lançado e o valor é: ${value} 🎲`);
+    return { value };
+}
+
+async function getRandomBlock() {
+    let random = Math.random();
+    let result;
+
+    switch (true) {
+        case random < 0.33:
+            result = "Reta";
+            break
+        case random < 0.66:
+            result = "Curva"
+            break
+        default:
+            result = "confronto"
+        
+
     }
+    return result;
 }
 
 (async function main() {
-
     //! await roda o primeira função e quando ela terminar, ele continua a execução do código com a funcão principal.
-    const {player1, player2} = await randomplayer();
-    console.log(`🏁 Corrida de Kart entre ${player1.name} e ${player2.name} Esta começando!... \n`);
+    console.log("🚀 Bem-vindo ao Mario Kart! 🚀");
+   
     await rounds();
-    
+      
 
 })()
