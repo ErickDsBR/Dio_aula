ca36795443b5173ea8fb7fd3ce015c7f4bc9f0ef
ErickDsBR <erick.brasil2102@gmail.com>
2026-03-14 11:48:43 -0300

    win / victory / draw

--- src/index.js
+++ src/index.js
@@ -87,33 +87,73 @@ async function rounds() {
             console.log("🚀 Reta! A velocidade é o fator mais importante! 🚀");
             await wait(3000);
             console.log(`🎲 ${player1.name} rolou um ${dicep1} 🎲`);
-            console.log(`Soma: ${player1.velocidade + dicep1} `);
+            const soma1 = player1.velocidade + dicep1;
+            console.log(`Soma: ${soma1} `);
             await wait(3500);
             console.log(`🎲 ${player2.name} rolou um ${dicep2} 🎲`);
-            console.log(`Soma: ${player2.velocidade + dicep2} `);
+            const soma2 = player2.velocidade + dicep2;
+            console.log(`Soma: ${soma2} `);
             
+            if(soma1 > soma2) {
+                console.log(`🏆 ${player1.name} venceu a rodada! 🏆`);
+                
+                if(soma1 < soma2) {
+                    console.log(`🏆 ${player2.name} venceu a rodada! 🏆`);
+                }
+            }
+            else {
+                console.log(`🤝 Empate! 🤝`);
+            }
+
         }
         if(result === "Curva") {
             await wait(2000);
             console.log("🔄 Curva! A manobrabilidade é o fator mais importante! 🔄");
             await(3000);
             console.log(`🎲 ${player1.name} rolou um ${dicep1} 🎲`);
-            console.log(`Soma: ${player1.manobrabilidade + dicep1} `);
+            const soma1 = player1.manobrabilidade + dicep1;
+            console.log(`Soma: ${soma1} `);
             await wait(3500);
             console.log(`🎲 ${player2.name} rolou um ${dicep2} 🎲`);
-            console.log(`Soma: ${player2.manobrabilidade + dicep2} `);
+            const soma2 = player2.manobrabilidade + dicep2;
+            console.log(`Soma: ${soma2} `);
+
+            if(soma1 > soma2) {
+                console.log(`🏆 ${player1.name} venceu a rodada! 🏆`);
+
+                if(soma1 < soma2) {
+                    console.log(`🏆 ${player2.name} venceu a rodada! 🏆`);
+                }
+            }
+            else {
+                console.log(`🤝 Empate! 🤝`);
+            }
         }
         else if(result === "confronto") {
             await wait(2000);
             console.log("⚔️ Confronto! O poder é o fator mais importante! ⚔️");
             await wait(3000);
             console.log(`🎲 ${player1.name} rolou um ${dicep1} 🎲`);
-            console.log(`Soma: ${player1.poder + dicep1} `);
+            const soma1 = player1.poder + dicep1;
+            console.log(`Soma: ${soma1} `);
             await wait(3500);
             console.log(`🎲 ${player2.name} rolou um ${dicep2} 🎲`); 
-            console.log(`Soma: ${player2.poder + dicep2} `);
+            const soma2 = player2.poder + dicep2;
+            console.log(`Soma: ${soma2} `);
+
+            if(soma1 > soma2) {
+                console.log(`🏆 ${player1.name} venceu a rodada! 🏆`);
+
+                if(soma1 < soma2) {
+                    console.log(`🏆 ${player2.name} venceu a rodada! 🏆`);
+                }
+            }
+            else {
+                console.log(`🤝 Empate! 🤝`);
+            }
 
         }
+        
 
     }
 }
